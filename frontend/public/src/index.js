5361cee8f3b241207602112da316ca5d5e0a89c2
HenriqueCalazanss <henriquecallazans@gmail.com>
2024-08-17 22:17:37 -0300

    1.2

--- frontend/public/src/index.js
+++ frontend/public/src/index.js
@@ -1,21 +1,35 @@
 // URL base da função do Netlify
 const FUNCTION_URL = '/.netlify/functions/functions';
 
-// PopUp da tela inicial, abrie e fechar contatos e como participar!
+// Função para desativar o scroll
+function disableScroll() {
+  document.body.style.overflow = 'hidden';
+}
+
+// Função para ativar o scroll
+function enableScroll() {
+  document.body.style.overflow = '';
+}
+
+// Função para abrir o popup
 function openPopup(popupId) {
   document.getElementById(popupId).style.display = 'flex';
+  disableScroll(); // Desativa o scroll quando o popup é aberto
 }
 
+// Função para fechar o popup
 function closePopup(popupId) {
   document.getElementById(popupId).style.display = 'none';
+  enableScroll(); // Reativa o scroll quando o popup é fechado
 }
 
-// Fechar o pop-up ao clicar fora do conteúdo
+// Fechar o popup ao clicar fora do conteúdo
 window.addEventListener('click', function(event) {
   const popupElements = document.querySelectorAll('.popup');
   popupElements.forEach(popup => {
-      if (event.target === popup) {
-          popup.style.display = 'none';
-      }
+    if (event.target === popup) {
+      popup.style.display = 'none';
+      enableScroll(); // Reativa o scroll ao fechar o popup
+    }
   });
 });
